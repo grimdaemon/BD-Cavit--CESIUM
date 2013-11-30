b9c8cd2bd3268eb86ed7923f69346e138433013a
Damien Balthassat <damien.balthassat@gmail.com>
2013-11-30 11:52:33 +0100

    Changement du fond de plan

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var main=function() { //launched when the document is ready
     "use strict"; //use strict javascript    
 
     //open viewer using NASA cool tiles
-    var viewer = new Cesium.CesiumWidget('cesiumContainer');
-
+    var viewer = new Cesium.CesiumWidget('cesiumContainer', {
+        imageryProvider : new Cesium.ArcGisMapServerImageryProvider({
+            url : 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer',
+            proxy : new Cesium.DefaultProxy('proxy/index.php?url=')
+        })
+    });
 };
