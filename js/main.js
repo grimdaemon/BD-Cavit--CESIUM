120d59f30ff1ac1d394eaec164092ceb683e34d7
Damien Balthassat <damien.balthassat@gmail.com>
2013-12-01 16:44:20 +0100

    Chargement des données en fonction de l'id

--- js/main.js
+++ js/main.js
@@ -6,16 +6,83 @@ var main=function() { //launched when the document is ready
     var ellipsoid;
 
     var pickCountry = function(id) {
+        // Loading the bouding box of the country
+        lib_ajax.get("data/bbox_dpt_wgs84.json", function(__data) {
+            var data = JSON.parse(__data).bbox_dpt_france[id];
+            if(data == undefined) {
+                alert("Ce département n'existe pas");
+                return;
+            } 
+            var extent = new Cesium.Extent(
+                Cesium.Math.toRadians(data["long_min"]),
+                Cesium.Math.toRadians(data["lat_min"]),
+                Cesium.Math.toRadians(data["long_max"]),
+                Cesium.Math.toRadians(data["lat_max"]));
+            scene.getAnimations().add(
+                Cesium.CameraFlightPath.createAnimationExtent(scene, 
+                {
+                    destination: extent
+                })
+            );
+        });
 
+        // Loading the data 
+        var file = lib_ajax.get("data/"+id+".json", function(__data) {
+            var data = JSON.parse(__data);
+
+            // Computing the data
+            var dataPoints = data.data;
+            htmlInteraction.getElement('loadingText').innerHTML = 'Chargement : 0 / ' + dataPoints.length;
+            var i = 0, limit = dataPoints.length, busy = false;
+            var process = window.setInterval(function() {
+                if(!busy) {
+                    busy = true;
+                    htmlInteraction.getElement('loadingText').innerHTML = 'Chargement : '+i+' / ' + dataPoints.length;
+                    var point = dataPoints[i]; 
+
+                    var lat = parseFloat(point["x_wgs84"]);
+                    var lon = parseFloat(point["y_wgs84"]);
+
+                    htmlInteraction.getElement("legend_"+point["type_cavite"]).disabled = false;
+
+                    if(point["type_cavite"] == "naturelle") {
+                        var t = new Thumbtrack(ellipsoid, lat, lon);
+                        var elements = t.getPrimitives();
+                        for(var p in elements) {
+                            var plot = scene.getPrimitives().add(elements[p]);
+                            plot.pickable = true;
+                            plot.pointIndex=i;
+                        }
+                    }
+                    if(++i >= limit) {
+                        window.clearInterval(process);
+                        htmlInteraction.getElement('loadingText').innerHTML = '';
+                    }
+                    busy = false;
+                }
+            }, 100);
+
+            //handle click
+            var handlerClick = new Cesium.ScreenSpaceEventHandler(scene.getCanvas());
+            handlerClick.setInputAction(function (movement) {
+                var pickedObject = scene.pick(movement.position);
+                if (!Cesium.defined(pickedObject)) return;
+                if (!pickedObject.primitive) return;
+                if (!pickedObject.primitive.pickable) return;
+                var point=dataPoints[pickedObject.primitive.pointIndex];
+                popup.open(point);
+                if (pickedObject.primitive.onclick) pickedObject.primitive.onclick(true);
+            }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+        });
     }
 
     var run = function() {
         // Events
         var formCountry = htmlInteraction.getElement("pickCountryForm");
         formCountry.addEventListener('submit', function(event) {
-            alert("ok");
-            return false;
-        }, false);
+            event.preventDefault();
+            pickCountry(htmlInteraction.getElement("countryNumber").value);
+        });
 
         //open viewer using NASA cool tiles
         viewer = new Cesium.CesiumWidget('cesiumContainer', {
@@ -32,8 +99,8 @@ var main=function() { //launched when the document is ready
 
 
         // Loading the bouding box of the france
-        /*lib_ajax.get("data/bouding_box_france.json", function(__data) {
-            var data = JSON.parse(__data);
+        lib_ajax.get("data/bbox_france_wgs84.json", function(__data) {
+            var data = JSON.parse(__data).bbox_france;
             var extent = new Cesium.Extent(
                 Cesium.Math.toRadians(data["long_min"]),
                 Cesium.Math.toRadians(data["lat_min"]),
@@ -45,10 +112,7 @@ var main=function() { //launched when the document is ready
                     destination: extent
                 })
             );
-        });*/
-
-        
-
+        });
 
         // Loading datas
         /*lib_ajax.get("data/01.json", function(__data) {
